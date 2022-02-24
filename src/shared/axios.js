a38f6a35d376a0f2634ebf8317aa1ae91114f935
han-woo-jin <97419805+han-woo-jin@users.noreply.github.com>
2022-02-24 20:34:07 +0900

    Update axios.js

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -6,7 +6,7 @@ const search = localStorage.getItem("search")
 
 export const instance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  baseURL: "http://13.124.244.126/",
+  baseURL: "",
   headers: {
     // "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     // accept: "*/*",
@@ -19,7 +19,7 @@ export const instance = axios.create({
 
 const forsearch = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  baseURL: "http://13.124.244.126/",
+  baseURL: "",
   headers: {
     // "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     // accept: "*/*",
